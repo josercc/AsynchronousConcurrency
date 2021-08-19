a84830624ecb85ee47b234c3a433a28c347d4bad
joser <joser@pineal.ai>
2021-08-19 09:37:10 +0800

    新增FlutterError支持LocalizedError协议

--- Sources/AsynchronousConcurrency/FutureError.swift
+++ Sources/AsynchronousConcurrency/FutureError.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 /// 错误处理
-public enum FutureError: Error {
+public enum FutureError: Error, LocalizedError {
     /// 未来值还没有加载完毕
     case futureNotReadly
     /// 系统错误
@@ -16,7 +16,7 @@ public enum FutureError: Error {
     /// 自定义其他错误类型
     case custom(String)
     
-    public var message:String {
+    public var errorDescription: String? {
         switch self {
         case .futureNotReadly:
             return "Future data is not ready, please use await synchronization to use"
